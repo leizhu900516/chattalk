36ca6e2d0e8449b199fc4ecf76cb581488db76ff
leizhu900516 <leizhu900516@163.com>
2019-06-07 09:46:59 +0800

    轮询算法更改，添加测试

--- utils/wrr.go
+++ utils/wrr.go
@@ -1,5 +1,8 @@
 package utils
+
 //加权轮询算法 weighted round robin
+// 客服权重默认都为0
+// 返回当前匹配到的客服信息
 
 
 
@@ -9,6 +12,27 @@ type Node struct {
 	Weight int
 }
 
-func Wrr(){
 
+func Wrr(customers []*Node)  (best *Node){
+	if len(customers) == 0{
+		return
+	}
+	weight_sum := 0
+	for _,node := range customers{
+		if node == nil{
+			return
+		}
+		weight_sum+=node.Weight
+		node.Current +=node.Weight
+		if best == nil || node.Current >best.Current{
+			best = node
+		}
+	}
+	if best == nil{
+		return
+	}
+	best.Current -= weight_sum
+
+	return
 }
+
---
+++ utils/wrr_test.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+//加权轮询算法 weighted round robin
+// 客服权重默认都为0
+// 返回当前匹配到的客服信息
+// go test -v
+/* 算法结果样例
+目标 a [5, 2, 1] [-3, 2, 1]
+目标 b [2, 4, 2] [2, -4, 2]
+目标 a [7, -2, 3] [-1, -2, 3]
+目标 a [4, 0, 4] [-4, 0, 4]
+目标 c [1, 2, 5] [1, 2, -3]
+目标 a [6, 4, -2] [-2, 4, -2]
+目标 b [3, 6, -1] [3, -2, -1]
+目标 a [8, 0, 0] [0, 0, 0]
+*/
+
+func TestWrr(t *testing.T)  {
+	nodes := []*Node{
+		&Node{"a",0,5},
+		&Node{"b",0,2},
+		&Node{"c",0,1},
+	}
+	for i:=0;i<8;i++{
+		best :=	Wrr(nodes)
+		fmt.Println(best.Uid,best)
+	}
+}
+
+
